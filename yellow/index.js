db2235b846c71087d4f7a8e79cd0f4d394da5127
qafrankly <31970630+qafrankly@users.noreply.github.com>
2018-01-31 02:26:52 -0500

    Add files via upload

--- yellow/index.js
+++ yellow/index.js
@@ -1,1 +1,16 @@
+import React, { Component, PropTypes } from 'react';
 
+class MultiDemo12 extends Component {
+	
+  render(){
+	  
+    return (
+      <div className='FranklySecondModulesDemo'>
+	      <h2 className='demo_text'>nad2: {this.props.text}</h2>
+      </div>
+    );
+	  
+  }
+}
+
+export default MultiDemo12;

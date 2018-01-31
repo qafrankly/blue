d097b2299bed577389b6650ec1cdfb12de3ef486
qafrankly <31970630+qafrankly@users.noreply.github.com>
2018-01-31 09:35:40 +0200

    Update index.js

--- purple/index.js
+++ purple/index.js
@@ -1,16 +1,16 @@
 import React, { Component, PropTypes } from 'react';
 
-class MultiDemo14 extends Component {
+class purple1 extends Component {
 	
   render(){
 	  
     return (
-      <div className='FranklyFourthModulesDemo'>
-	      <h2 className='demo_text'>nad4: {this.props.text}</h2>
+      <div className='Fourth'>
+	      <h2 className='demo_text'>purple: {this.props.text}</h2>
       </div>
     );
 	  
   }
 }
 
-export default MultiDemo14;
+export default purple1;

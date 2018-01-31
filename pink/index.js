f58e7ce779520e683f9dbe5c06fd070d8ec7069f
qafrankly <31970630+qafrankly@users.noreply.github.com>
2018-01-31 02:32:36 -0500

    Add files via upload

--- pink/index.js
+++ pink/index.js
@@ -1,1 +1,16 @@
+import React, { Component, PropTypes } from 'react';
 
+class MultiDemo13 extends Component {
+	
+  render(){
+	  
+    return (
+      <div className='FranklyThirdModulesDemo'>
+	      <h2 className='demo_text'>nad3: {this.props.text}</h2>
+      </div>
+    );
+	  
+  }
+}
+
+export default MultiDemo13;
